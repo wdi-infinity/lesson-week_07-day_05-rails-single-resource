c5d671bffc06337ff262738f60353d83307ae5ba
sara saleh alofi <saraalawfi1995@gmail.com>
2019-03-03 15:22:57 +0300

    Done

--- library/app/controllers/books_controller.rb
+++ library/app/controllers/books_controller.rb
@@ -5,4 +5,36 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
     end
+    def new
+
+    end
+    def destroy
+        @book = Book.find(params[:id])
+       @book.destroy
+        redirect_to books_path 
+    end
+    def edit
+         @book = Book.find(params[:id])
+    end
+     def update
+       @book = Book.find(params[:id])
+       if @book.update(book_params)
+        redirect_to books_path 
+       else
+         render 'edit'
+       end
+
+    end
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            redirect_to books_path
+        else
+            render 'new'
+        end
+    end
+        def book_params
+            params.require(:book).permit(:title, :author)
+    end
+
 end
--- library/config/routes.rb
+++ library/config/routes.rb
@@ -1,9 +1,27 @@
 Rails.application.routes.draw do
-  get '/books' => 'books#index', as: 'books'
-  get '/books/:id' => 'books#show', as: 'book'
+  # get '/books' => 'books#index', as: 'books' #books_path
+  #  get '/books/new' => "books#new", as:'new_book'
+  # get '/books/:id/edit' => "books#edit", as:'edit_book'
+   
+  # get '/books/:id' => 'books#show', as: 'book' #book_path(book)
+
+  # patch 'books/:id' => 'books#update'
+
+  # post '/books' => "books#create" # user request
+  # delete 'books/:id' => "books#destroy"
+
+  resources :books
+
+  # get '/movies' => 'movies#index',as: 'movies'
+  # get '/movies/new' => "movies#new", as:'new_movie'
+  #  get '/movies/:id/edit' => "movies#edit",  as:'edit_movie'
+  # get '/movies/:id' => 'movies#show',as: 'movie'
+  # patch 'movies/:id' => 'movies#update'
+  #  post '/movies' => "movies#create" # user request
+  #  delete 'movies/:id' => "movies#destroy"
+  resources :movies
   
-  get '/movies' => 'movies#index',as: 'movies'
-  get '/movies/:id' => 'movies#show',as: 'movie'
+
 
   get '/ingredients' => 'ingredients#index', as:'ingredients'
   get '/ingredients/:id' => 'ingredients#show', as: 'ingredient'
