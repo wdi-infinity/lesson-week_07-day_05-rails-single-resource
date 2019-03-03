e1caf1bc8f2276b63354e880a5eb4aca513feb76
Majd Saleh <mjd.s.m@hotmail.com>
2019-03-03 15:48:41 +0300

    add paintings for Books and Movies

--- library/app/controllers/movies_controller.rb
+++ library/app/controllers/movies_controller.rb
@@ -14,7 +14,19 @@ class MoviesController < ApplicationController
     end
 
     def new 
+    end
 
+    def edit 
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+         @movie = Movie.find(params[:id])
+         if @movie.update(movie_params)
+            redirect_to movies_path
+        else
+            render "edit"
+        end
     end
 
     def create 
--- library/db/schema.rb
+++ library/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2019_03_03_122820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 1) do
     t.string "director"
     t.string "title"
     t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "paintings", force: :cascade do |t|
+    t.string "title"
+    t.string "artist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
