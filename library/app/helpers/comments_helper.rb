50abaf13549f7d79b188a335112034dbf893531c
Abdullah <ab4ullahkh@gmail.com>
2019-03-03 20:52:58 +0300

    HW articles

---
+++ library/app/helpers/comments_helper.rb
@@ -0,0 +1,16 @@
+# module CommentsHelper
+# end
+
+
+class CommentsController < ApplicationController
+    def create
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.create(comment_params)
+      redirect_to article_path(@article)
+    end
+   
+    private
+      def comment_params
+        params.require(:comment).permit(:commenter, :body)
+      end
+  end
