48400e564688fe4b11b6d0ea5e9d78c8990a05d7
ceahmad <almansour.a.ahmad@gmail.com>
2019-03-03 14:51:09 +0300

    add library of movies and books

---
+++ library/app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+
+    def index
+        
+
+        @books = Book.all
+        
+    end
+
+    def show
+        
+        @book = Book.find(params[:id])
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
+
+    def new
+        
+        # @book = Book.find(params[:id])
+    end
+
+    def edit 
+        @book = Book.find(params[:id])
+
+    end
+
+    def update
+        @book = Book.find(params[:id]) 
+        if @book.update(book_params)
+            redirect_to books_path
+        else
+            render 'edit'
+
+        end
+    end
+
+
+    def create
+        
+         @book = Book.new(book_params)
+           if @book.save 
+            redirect_to books_path
+           else
+            render action: "new"
+           end
+    end
+
+    def book_params
+        params.require(:book).permit(:title, :author)
+    end
+end
---
+++ library/config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/books' => "books#index" , as:'books' #books_path
+  get '/books/new' => "books#new" , as: 'new_book'
+  get '/books/:id/edit' => "books#edit" , as:'edit_book'
+  get '/books/:id' => "books#show" , as: 'book' #book_path(book)
+  get '/movies' => "movies#index" , as: 'movies'
+  get '/movies/new' => "movies#new" , as: 'new_movie'
+  get '/movies/:id/edit' => "movies#edit" , as:'edit_movie'
+  get '/movies/:id' => "movies#show" , as: 'movie'
+  get '/ingredients' => "ingredients#index" , as: 'ingredients'
+  get '/ingredients/:id' => "ingredients#show" , as: 'ingredient' 
+  
+  
+   post '/books' => "books#create" 
+   post '/movies' => "movies#create"
+  patch '/books/:id' => "books#update"
+  patch '/movies/:id' => "movies#update"
+   delete '/books/:id' => "books#destroy"
+   delete '/movies/:id' => "movies#destroy"
+ 
+  
+end
