bed9bda7560e93506671216bf57f3beccd19742d
Majd Saleh <mjd.s.m@hotmail.com>
2019-03-03 19:39:04 +0300

    HomeWork submission - Add articles that can have comments

--- library/db/schema.rb
+++ library/db/schema.rb
@@ -10,16 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_122820) do
+ActiveRecord::Schema.define(version: 2019_03_03_133712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -37,4 +53,5 @@ ActiveRecord::Schema.define(version: 2019_03_03_122820) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
