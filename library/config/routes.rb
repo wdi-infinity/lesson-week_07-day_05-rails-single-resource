05cca8a000091526cd01616d4fccea2e349f3275
micfin <michael.finneran@generalassemb.ly>
2019-03-03 14:46:21 +0300

    Add CRUD for books

---
+++ library/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/books' => "books#index", as: 'books' # books_path
+  get '/books/new' => "books#new", as: 'new_book'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  get '/books/:id' => "books#show", as: 'book' # book_path(book)
+  post '/books' => "books#create"
+  patch '/books/:id' => "books#update"
+  delete '/books/:id' => "books#destroy"
+end
