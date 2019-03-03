01aee341b2a8c888fe1d6d023ffcb139c6d01d7a
loltisha <alyahadiem@gmail.com>
2019-03-03 13:52:07 +0300

    commit

--- library/app/controllers/books_controller.rb
+++ library/app/controllers/books_controller.rb
@@ -7,4 +7,27 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])    
     end
+    def destroy
+       @book = Book.find(params[:id]) 
+       @book.destroy  
+       redirect_to books_path
+    end
+
+    def new
+
+    end
+
+    def create
+    # Book.createparams[:book] #create new book
+      @book = Book.new(books_params) #create new instance but it is not saves into the database (it like a copy)
+      if @book.save #save the data into the database
+        redirect_to books_path
+      else
+        render "new"
+      end
+    end
+
+    def books_params
+        params.require(:book).permit(:title, :author)
+    end
 end
--- library/app/controllers/movies_controller.rb
+++ library/app/controllers/movies_controller.rb
@@ -6,4 +6,21 @@ class MoviesController < ApplicationController
     def show
         @movie = Movie.find(params[:id])    
     end
+    def new
+
+    end
+
+    def create
+
+        @movie = Movie.new(movies_params) #create new instance but it is not saves into the database (it like a copy)
+      if @movie.save #save the data into the database
+        redirect_to movies_path
+      else
+        render "new"
+      end
+    end
+
+    def movies_params
+        params.require(:movie).permit(:director, :title, :rating)
+    end
 end
