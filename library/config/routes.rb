4c3430f3ab65f15ae58c61bdfd5d20945f4fc5f3
badraih <aaa@aAas-MacBook-Pro-2.local>
2019-03-03 19:37:49 +0300

    Homework... I solve the homework, but I need to review it again

--- library/config/routes.rb
+++ library/config/routes.rb
@@ -1,12 +1,31 @@
 Rails.application.routes.draw do
+  # resources :articles
+  resources :articles do
+    resources :comments
+  end
+  # resources :paintings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/books' => "books#index", as: 'books'
-  # first named router the books is controller and the index is method inside books controller 
-  get '/books/:id' => "books#show", as: 'book'
+  # get '/books' => "books#index", as: 'books'
+  # # first named router the books is controller and the index is method inside books controller 
+  # get '/books/new' => "books#new", as:'new_book'# order important before id
+  # get '/books/:id/edit'=> "books#edit", as:'edit_book'
+  # get '/books/:id' => "books#show", as: 'book'# id
+  # post '/books' => "books#create"
+  # patch '/books/:id' => "books#update"
+  # delete '/books/:id' => "books#destroy"
+  resources :books
+
   #movies
-  get '/movies' => "movies#index", as: 'movies'
-  get '/movies/:id' => "movies#show", as: 'movie'
+  # get '/movies' => "movies#index", as: 'movies'
+  # get '/movies/new' => "movies#new", as: 'new_movie'
+  # get '/movies/:id/edit' => "movies#edit", as: 'edit_movie'
+  # get '/movies/:id' => "movies#show", as: 'movie'
+  # post '/movies' => "movies#create"
+  # patch '/movies/:id'=> "movies#update"
+  # delete '/movies/:id'=>"movies#destroy"
+  resources :movies
   # ingredients
-  get '/ingredients' => "ingredients#index", as:'ingredients'
-  get '/ingredients/:id' => "ingredients#show", as:'ingredient'
+  # get '/ingredients' => "ingredients#index", as:'ingredients'
+  # get '/ingredients/:id' => "ingredients#show", as:'ingredient'
+  resources :ingredients
 end
