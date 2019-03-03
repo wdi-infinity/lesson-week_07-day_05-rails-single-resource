58fb577d22884237697f4a89a1b234fb43d82573
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-03-03 15:21:21 +0300

    last

--- library/config/routes.rb
+++ library/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get '/books' => "books#index", as:'books'  #books_path
-  get '/books/new'=> "books#new" , as:'new_book' 
-  get '/books/:id' => 'books#show', as: 'book' #book_path(book)
-  post '/books' => "books#create"
-  patch '/books/:id' => "books#update"
-  delete '/books/:id' => "books#destroy"
-  get '/books/:id/edit' => "books#edit", as:'edit_book'
+  # get '/books' => "books#index", as:'books'  #books_path
+  # get '/books/new'=> "books#new" , as:'new_book' 
+  # get '/books/:id' => 'books#show', as: 'book' #book_path(book)
+  # post '/books' => "books#create"
+  # patch '/books/:id' => "books#update"
+  # delete '/books/:id' => "books#destroy"
+  # get '/books/:id/edit' => "books#edit", as:'edit_book'
+  resources :books
    get  'movies' => "movies#index", as:'movies'
    get '/movies/new'=> "movies#new" , as:'new_movie'
    get '/movies/:id' => 'movies#show', as: 'movie'
